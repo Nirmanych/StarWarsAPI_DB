d2a66c924e2acdc172666a44aec4667c2d28ce5f
Nirmanych <nursnirma@gmail.com>
2025-03-04 23:10:01 +0500

    Step 8: Adding Row component and changing render view.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ItemList from '../item-list'
 import PersonDetails from '../person-details'
+
+import './app.css'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 import SwapiService from "../../services/swapi-service";
 
-import './app.css'
-
-export default class App extends Component {
+export default class App extends Component{
 
     swapiService = new SwapiService()
 
@@ -31,8 +31,8 @@ export default class App extends Component {
 
         return (
             <div>
-                <Header />
-                <RandomPlanet />
+                <Header/>
+                <RandomPlanet/>
 
                 <PeoplePage />
 
@@ -40,7 +40,8 @@ export default class App extends Component {
                     <div className="col-md-6">
                         <ItemList
                             onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllPlanets} />
+                            getData={ this.swapiService.getAllPlanets }
+                            renderItem={ (item) => item.name }/>
                     </div>
                     <div className="col-md-6">
                         <PersonDetails personId={this.state.selectedPerson} />
@@ -51,7 +52,8 @@ export default class App extends Component {
                     <div className="col-md-6">
                         <ItemList
                             onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllStarships} />
+                            getData={ this.swapiService.getAllStarships }
+                            renderItem={ (item) => item.name }/>
                     </div>
                     <div className="col-md-6">
                         <PersonDetails personId={this.state.selectedPerson} />
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
+
 import './item-list.css'
 import Spinner from "../spinner";
 
 export default class ItemList extends Component {
-
 
     state = {
         itemList: null
@@ -20,24 +20,30 @@ export default class ItemList extends Component {
     }
 
     renderItems(arr) {
-        return arr.map(({ id, name }) => {
+        return arr.map((item) => {
+            const { id } = item
+            const label = this.props.renderItem(item)
             return (
                 <li className="list-group-item"
-                    key={id}
-                    onClick={() => this.props.onItemSelected(id)}>
-                    {name}
+                    key={ id }
+                    onClick={ () => this.props.onItemSelected(id) }>
+                    { label }
                 </li>
             )
         })
     }
 
     render() {
+
         const { itemList } = this.state
+
         if (!itemList) return <Spinner />
+
         const items = this.renderItems(itemList)
+
         return (
             <ul className="item-list list-group">
-                {items}
+                { items }
             </ul>
         )
     }
